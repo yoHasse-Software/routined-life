050a7da1a3ee69fecd7fcf75cf5a8a39576c3ad0
Marcus Nätteldal <marcus.natteldal@ltu.se>
2025-09-09 23:12:21 +0200

    updates

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-export * from './types.d';
+export * from './types';
 export * from './Utilities';
 export * from './RoutineTemplates';
 export { dataStore } from './DataStoreService';
