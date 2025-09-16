45fda7686b6cf8c362f2c3af7e659fcf7482164a
Marcus Nätteldal <marcus.natteldal@outlook.com>
2025-09-16 23:02:21 +0200

    add: capacitor

---
+++ android/app/src/main/java/se/yohasse/routinedlife/MainActivity.java
@@ -0,0 +1,5 @@
+package se.yohasse.routinedlife;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'se.yohasse.routinedlife',
+  appName: 'routined-life',
+  webDir: 'dist'
+};
+
+export default config;
